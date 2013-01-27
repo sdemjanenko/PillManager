72fcb8fd6377b0cccbf2bb7583830daf42a7eb70
Stephen Demjanenko <sdemjanenko@gmail.com>
2013-01-26 16:00:48 -0800

    Add Pill & PillCollection Constructors

---
+++ pill_manager.js
@@ -0,0 +1,56 @@
+var Pill = (function($) {
+  var skeleton = $("<div class='pill'><span class='value'></span><span class='close'>x</span><div/>");
+
+  var ctor = function(container, value) {
+    var _this = this;
+    _this.$el = skeleton.clone();
+
+    _this.value = value;
+    _this.changeValue(value, {silent: true});
+    _this.$el.on('click', '.close', function() { _this.destroy.call(_this); });
+
+    container.append(this.$el);
+  };
+
+  ctor.prototype = {
+    changeValue: function(new_value, options) {
+      this.value = new_value;
+      this.$el.find('.value').text(new_value);
+      if (options && !options.silent)
+        $(this).trigger('change', [new_value]);
+    },
+    destroy: function() {
+      this.$el.remove();
+      $(this).trigger('destroyed');
+      $(this).off();
+    }
+  };
+
+  return ctor;
+})(jQuery);
+
+var PillCollection = (function($) {
+  var ctor = function(container) {
+    this.pills = [];
+    this.container = container;
+  };
+
+  ctor.prototype = {
+    addPill: function(value) {
+      var new_pill = new Pill(this.container, value);
+      var _this = this;
+      $(new_pill).on('destroyed', function() { _this.removePill(new_pill); });
+      this.pills.push(new_pill);
+    },
+    removePill: function(pill) {
+      var index = this.pills.indexOf(pill);
+      if (index !== -1)
+        this.pills.splice(index, 1);
+    },
+    value: function() {
+      return $.map(this.pills, function(pill) { return pill.value; });
+    }
+  };
+
+  return ctor;
+})(jQuery);
